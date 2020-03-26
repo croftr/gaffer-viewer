fedc66b5241ba8d4a23b48df53230633ed9da17f
croftr <rob@robincroft.com>
2020-03-26 17:33:08 +0000

    ui render of graph

--- src/Data.jsx
+++ src/Data.jsx
@@ -14,10 +14,10 @@ import busCompany from "./images/busCompany.png";
 export default function Data() {
 
     const [data, setData] = React.useState([]);
+    const [graph, setGraph] = React.useState([]);
     const [isShowGraph, setIsShowGraph] = React.useState(false);
 
     const standard = async () => {
-
 
         setIsShowGraph(false);
         const data = await execute(
@@ -26,7 +26,13 @@ export default function Data() {
                 "limit": 1000
             }
         );
+
+        console.log("data 1 ", data);
+
+
         setData(data);
+
+        console.log("data 2 ", data);
 
     }
 
@@ -58,69 +64,190 @@ export default function Data() {
         setData(data);
     }
 
-    const visGraph = async () => {
+    // const visGraph = async () => {
+
+    //     setIsShowGraph(true);
+
+    //     const vis = window.vis;
+
+    //     const responseData = await execute(
+    //         {
+    //             "class": "uk.gov.gchq.gaffer.operation.OperationChain",
+    //             "operations": [
+    //                 {
+    //                     "class": "uk.gov.gchq.gaffer.operation.impl.get.GetAllElements"
+    //                 },
+    //                 {
+    //                     "class": "uk.gov.gchq.gaffer.operation.impl.generate.GenerateObjects",
+    //                     "elementGenerator": {
+    //                         "class": "uk.gov.gchq.gaffer.layouts.vis.VisGraphGenerator"
+    //                     }
+    //                 }
+    //             ]
+    //         }
+    //     );
+
+
+    //     const nodes = responseData.filter(element => element.id).map(node => {
+
+    //         const group = node.group.split("|")[0];
+    //         const subType = node.group.split("|")[1];
+
+    //         let image;
+
+    //         if (group === "company") {
+    //             image = busCompany;
+    //         } else {
+    //             if (subType === "big_bus") {
+    //                 image = bus2;
+    //             } else {
+    //                 image = bus1;
+    //             }
+    //         }
+
+    //         return {
+    //             image,
+    //             ...node
+    //         }
+    //     });
+
+    //     const edges = responseData.filter(element => element.from);
+
+    //     console.log("got nodes", nodes);
+    //     // console.log("got edges", edges);
+
+    //     var visNodes = new vis.DataSet(nodes);
+    //     var visEdges = new vis.DataSet(edges);
+
+    //     // create a network
+    //     var container = document.getElementById("mynetwork");
+    //     var data = {
+    //         nodes: visNodes,
+    //         edges: visEdges
+    //     };
+
+    //     var options = {
+    //         autoResize: true,
+    //         height: '100%',
+    //         width: '100%',
+    //         locale: 'en',
+    //         clickToUse: false,
+    //         interaction: {
+    //             navigationButtons: true,
+    //         }
+
+    //     }
+
+    //     var network = new vis.Network(container, data, options);
+
+    //     // setData(edges);
+
+    // }
+
+    const mapVisNode = (node) => {
+
+        let image, shape;
+
+        if (node.type === "company") {
+            image = busCompany;
+            shape = "image";
+        } else if (node.subType === "big_bus") {
+            image = bus2;
+            shape = "image";
+        } else if (node.subType === "small_bus") {
+            image = bus1;
+            shape = "image";
+        } else {
+            shape = "ellipse";
+        }
+
+        const visNode = {
+            group: node.type,
+            subType: node.subType,
+            id: node.type + node.subType + node.value,
+            label: node.value,
+            group: node.type,
+            title: node.subType,
+            shape,
+            image,
+            physics: false,
+        }
+
+        visNode.image = image;
+
+        return visNode;
+
+    }
+
+    const mapVisEdge = (edge) => {
+
+        const source = edge.source["uk.gov.gchq.gaffer.types.TypeSubTypeValue"];
+        const dest = edge.destination["uk.gov.gchq.gaffer.types.TypeSubTypeValue"];
+
+        let color = "grey";
+
+        switch (edge.group) {
+            case "crossesPaths":
+                color = "red";
+                break;
+            case "isPartOfThisCompany":
+                color = "purple";
+                break;
+            case "inTheSameBusCompany":
+                color = "lightBlue";
+                break;
+        }
+
+        const visEdge = {
+            from: source.type + source.subType + source.value,
+            to: dest.type + dest.subType + dest.value,
+            title: edge.group,
+            color,
+            arrows: edge.directed ? "to" : undefined
+        };
+
+        return visEdge;
+    }
+
+    const visGraphUi = () => {
 
         setIsShowGraph(true);
 
         const vis = window.vis;
+        const nodes = [];
+        const edges = [];
 
-        const responseData = await execute(
-            {
-                "class": "uk.gov.gchq.gaffer.operation.OperationChain",
-                "operations": [
-                    {
-                        "class": "uk.gov.gchq.gaffer.operation.impl.get.GetAllElements"
-                    },
-                    {
-                        "class": "uk.gov.gchq.gaffer.operation.impl.generate.GenerateObjects",
-                        "elementGenerator": {
-                            "class": "uk.gov.gchq.gaffer.layouts.vis.VisGraphGenerator"
-                        }
-                    }
-                ]
+        data.forEach(edge => {
+
+            console.log("edge ", edge);
+
+            const sourceNode = mapVisNode(edge.source["uk.gov.gchq.gaffer.types.TypeSubTypeValue"]);
+            const destNode = mapVisNode(edge.destination["uk.gov.gchq.gaffer.types.TypeSubTypeValue"]);
+            const visEdge = mapVisEdge(edge);
+
+            if (!nodes.find(i => i.id === sourceNode.id)) {
+                nodes.push(sourceNode);
             }
-        );
-        
 
-        const nodes = responseData.filter(element => element.id).map(node => {    
-                        
-            const group = node.group.split("|")[0];
-            const subType = node.group.split("|")[1];
-
-            let image;
-
-            if (group === "company") {
-                image = busCompany;
-            } else {
-                if (subType === "big_bus") {
-                    image = bus2;
-                } else {
-                    image = bus1;
-                }
+            if (!nodes.find(i => i.id === destNode.id)) {
+                nodes.push(destNode);
             }
-            
 
-            return {
-                chosen: {
-                    color: "yellow"
-                },
-                shape: "image",
-                image,
-                ...node
-            }
+            edges.push(visEdge)
+
         });
-
-        const edges = responseData.filter(element => element.from);
 
         console.log("got nodes", nodes);
         console.log("got edges", edges);
+
 
         var visNodes = new vis.DataSet(nodes);
         var visEdges = new vis.DataSet(edges);
 
         // create a network
         var container = document.getElementById("mynetwork");
-        var data = {
+
+        var visData = {
             nodes: visNodes,
             edges: visEdges
         };
@@ -131,29 +258,30 @@ export default function Data() {
             width: '100%',
             locale: 'en',
             clickToUse: false,
+            interaction: {
+                navigationButtons: true,
+            }
         }
 
-        var network = new vis.Network(container, data, options);
-
-        setData(edges);
+        var network = new vis.Network(container, visData, options);
 
     }
 
 
     return (
         <Paper>
-            <div style={{ display: "flex", alignItems: "center", padding: 8 }}>                
-                <Button style={{ marginLeft: 16, width: 200 }} variant="contained" onClick={standard}>Raw</Button>                
-                <Button style={{ marginLeft: 16, width: 200 }} variant="contained" onClick={visGraph}>Graph</Button>
+            <div style={{ display: "flex", alignItems: "center", padding: 8 }}>
+                <Button style={{ marginLeft: 16, width: 200 }} variant="contained" onClick={standard}>Raw</Button>
+                {/* <Button style={{ marginLeft: 16, width: 200 }} variant="contained" onClick={visGraph}>Graph</Button> */}
+                <Button style={{ marginLeft: 16, width: 200 }} variant="contained" onClick={visGraphUi}>Graph</Button>
             </div>
 
 
-            {!isShowGraph && <div style={{}}>
-                {data && <JSONPretty id="json-pretty" data={data} theme={JSONPrettyMon}></JSONPretty>}
+            <div style={{}}>
+                {!isShowGraph && data && <JSONPretty id="json-pretty" data={data} theme={JSONPrettyMon}></JSONPretty>}
+                <div id="mynetwork" style={{ width: "100%", height: !isShowGraph ? 0 : "calc(100vh - 140px)", border: "1px solid lightgray" }}></div>
             </div>
-            }
 
-            {isShowGraph && <div id="mynetwork" style={{ width: "100%", height: "calc(100vh - 140px)", border: "1px solid lightgray" }}></div>}
 
         </Paper>
     );
