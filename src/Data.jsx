84b33d6cad3b0952204acfde9e52f44dde5d47de
croftr <rob@robincroft.com>
2020-03-27 15:04:06 +0000

    add tabs

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,17 +4,29 @@ import JSONPretty from 'react-json-pretty';
 import JSONPrettyMon from 'react-json-pretty/dist/monikai'
 import Paper from '@material-ui/core/Paper';
 
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 import { convertRaw } from "./queries/raw.js"
 import { convertVis } from "./queries/visGraph.js"
 import { getEdgeColor } from "./utils/schamUtils"
 
+const views = [
+    "Raw",
+    "Graph",
+    "Payload"
+]
+
 export default function Data({ edgeTypes }) {
 
     const [data, setData] = React.useState([]);
-    const [isShowGraph, setIsShowGraph] = React.useState(false);
-    const [payload, setPayload] = React.useState({});
-    const [showPayload, setShowPayload] = React.useState(false);
-    
+    const [payload, setPayload] = React.useState({});    
+    const [tabPage, setTabPage] = React.useState(0);
+
+    const handleTabChange = (event, value) => {
+        value === 1 && visGraph();
+        setTabPage(value);
+    }
 
     const getData = async (edgeType) => {
         const { rawData, payload } = await convertRaw(edgeType);
@@ -32,38 +44,37 @@ export default function Data({ edgeTypes }) {
     }, []);
 
     return (
-        <Paper>
-            <div style={{ display: "flex", alignItems: "center", padding: 8, justifyContent: "space-between" }}>
-                <div id="dataViews">
-                    <Button style={{ marginLeft: 0, width: 100 }} variant="contained" onClick={() => { setIsShowGraph(false); setShowPayload(false) }}>Raw</Button>
-                    <Button style={{ marginLeft: 16, width: 100 }} variant="contained" onClick={() => { setIsShowGraph(true); setShowPayload(false); visGraph() }}>Graph</Button>
-                    <Button style={{ marginLeft: 16, width: 100 }} variant="contained" onClick={() => {setIsShowGraph(false); setShowPayload(true); }}>Payload</Button>
-                </div>
+        <Paper style={{ padding: 8 }}>
 
-                <div id="dataFilters" style={{}} >
-                    {edgeTypes.map(edgeType => (
-                        <Button
-                            color="primary"
-                            variant="contained"
-                            onClick={() => getData(edgeType)} style={{ marginRight: 8, backgroundColor: getEdgeColor(edgeType) }}
-                        >
-                            {edgeType}
-                        </Button>
-                    ))}
+            <Tabs value={tabPage} onChange={handleTabChange} aria-label="simple tabs example">
+                {views.map(view => <Tab label={view} />)}
+            </Tabs>
+
+            <div id="dataFilters" style={{ padding: 8 }} >
+                {edgeTypes.map(edgeType => (
                     <Button
                         color="primary"
-                        variant="contained" 
-                        onClick={() => getData()} style={{ marginRight: 8 }}
+                        variant="contained"
+                        onClick={() => getData(edgeType)} style={{ marginRight: 8, backgroundColor: getEdgeColor(edgeType) }}
                     >
-                        All
+                        {edgeType}
                     </Button>
-                </div>
+                ))}
+                <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={() => getData()} style={{ marginRight: 8 }}
+                >
+                    All
+                    </Button>
             </div>
 
+
             <div style={{}}>
-                {!isShowGraph && !showPayload && <JSONPretty id="json-pretty" data={data} theme={JSONPrettyMon}></JSONPretty>}
-                {showPayload && <JSONPretty id="json-pretty" data={payload}></JSONPretty> }
-                <div id="mynetwork" style={{ width: "100%", height: !isShowGraph && !showPayload ? 0 : "calc(100vh - 140px)", border: "1px solid lightgray" }}></div>
+                {tabPage === 0 && <JSONPretty id="json-pretty" data={data} theme={JSONPrettyMon}></JSONPretty>}
+                <div id="mynetwork" style={{ width: "100%", height: tabPage === 1 ? "calc(100vh - 200px)" : 0, border: "1px solid lightgray" }}></div> 
+                {tabPage === 2 && <JSONPretty id="json-pretty" data={payload}></JSONPretty>}
+                
             </div>
         </Paper>
     );
