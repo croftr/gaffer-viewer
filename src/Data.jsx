6fe0f43fc939c5a69b34eb6d492195336ca2dc87
croftr <rob@robincroft.com>
2020-03-27 16:07:08 +0000

    allow submit of payload

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Paper, Button, Tab, Tabs } from '@material-ui/core';
+import { Paper, Button, Tab, Tabs, TextField, Typography } from '@material-ui/core';
 import JSONPretty from 'react-json-pretty';
 import JSONPrettyMon from 'react-json-pretty/dist/monikai'
 import { convertRaw } from "./queries/raw.js"
 import { convertVis } from "./queries/visGraph.js"
 import { getEdgeColor } from "./utils/schamUtils"
 
+import ErrorIcon from '@material-ui/icons/Error';
+import SuccessIcon from '@material-ui/icons/CheckCircle';
+
+import { execute } from "./actions/GafferActions"
+
 const views = [
     "Raw",
     "Graph",
-    "Payload"
+    "Submit Payload",
+    "View Payload"
 ]
 
 export default function Data({ edgeTypes }) {
 
     const [data, setData] = React.useState([]);
-    const [payload, setPayload] = React.useState({});    
+    const [payload, setPayload] = React.useState({});
     const [tabPage, setTabPage] = React.useState(0);
+    const [payloadText, setPayloadText] = React.useState();
+    const [responseStatus, setResponseStatus] = React.useState();
 
     const handleTabChange = (event, value) => {
         value === 1 && visGraph();
@@ -32,6 +40,25 @@ export default function Data({ edgeTypes }) {
 
     const visGraph = () => {
         convertVis(data);
+    }
+
+    const submitPayload = async () => {
+        setResponseStatus(undefined);
+
+        try {
+            const rawData = await execute(JSON.parse(payloadText));
+            setData(rawData);
+            setPayload(payloadText);
+            convertVis(rawData);
+            setResponseStatus({ error: false, message: "Success" });
+        } catch (e) {
+            console.log("oops", e);
+            setResponseStatus({ error: true, message: e.message });
+        }
+    }
+
+    const onChangeText = (e) => {
+        setPayloadText(e.target.value);
     }
 
     React.useEffect(() => {
@@ -66,11 +93,33 @@ export default function Data({ edgeTypes }) {
                 </Button>
             </div>
 
-
             <div style={{}}>
                 {tabPage === 0 && <JSONPretty id="json-pretty" data={data} theme={JSONPrettyMon}></JSONPretty>}
-                <div id="mynetwork" style={{ width: "100%", height: tabPage === 1 ? "calc(100vh - 200px)" : 0, border: "1px solid lightgray" }}></div> 
-                {tabPage === 2 && <JSONPretty id="json-pretty" data={payload}></JSONPretty>}                
+                <div id="mynetwork" style={{ width: "100%", height: tabPage === 1 ? "calc(100vh - 200px)" : 0, border: "1px solid lightgray" }}></div>
+                {tabPage === 2 && (
+                    <div style={{ padding: 16 }}>
+                        <TextField
+                            value={payloadText}
+                            onChange={onChangeText}
+                            id="standard-textarea"
+                            label="Submit payload"
+                            variant="outlined"
+                            multiline
+                            fullWidth
+                            rows={20}
+                        />
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                            <Button onClick={submitPayload} style={{ marginTop: 16, marginRight: 16 }} color="primary" variant="contained">Submit</Button>
+                            {responseStatus && responseStatus.error && <span style={{ display: "flex", alignItems: "center", marginTop: 16 }}><ErrorIcon style={{ color: "red", marginRight: 8 }} /> {responseStatus.message}</span>}
+                            {responseStatus && !responseStatus.error && <span style={{ display: "flex", alignItems: "center", marginTop: 16 }}><SuccessIcon style={{ color: "green", marginRight: 8 }} /> {responseStatus.message}</span>}
+                        </div>
+                    </div>
+                )}
+                {tabPage === 3 && (
+                    
+                        <JSONPretty id="json-pretty" data={payload}></JSONPretty>
+                    
+                )}
             </div>
         </Paper>
     );
