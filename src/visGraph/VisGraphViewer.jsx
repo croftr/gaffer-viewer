a6694e4478e3e6e06f28a132911c945f373c4d2c
croftr <rob@robincroft.com>
2020-03-31 14:25:25 +0100

    move selected node text to bottom

--- src/visGraph/VisGraphViewer.jsx
+++ src/visGraph/VisGraphViewer.jsx
@@ -11,20 +11,23 @@ export default function VisGraph({ graphData, network }) {
     }
 
     return (
-        <div style={{ position: "absolute", top: 0, marginTop: 185, marginLeft: 4, zIndex: 999, display: "flex", alignItems: "center", justifyContent: "space-between" }} id="graphTools">
+        <React.Fragment>
+            <div style={{ position: "absolute", top: 0, marginTop: 185, marginLeft: 4, zIndex: 999, display: "flex", alignItems: "center", justifyContent: "space-between" }} id="graphTools">
 
-            <div id="tools" style={{ display: "flex", alignItems: "center" }}>
-                <Tooltip title="re-arrange">
-                    <IconButton onClick={arrange}>
-                        <ArrangeIcon />
-                    </IconButton>
-                </Tooltip>
+                <div id="tools" style={{ display: "flex", alignItems: "center" }}>
+                    <Tooltip title="re-arrange">
+                        <IconButton onClick={arrange}>
+                            <ArrangeIcon />
+                        </IconButton>
+                    </Tooltip>
+
+                </div>
 
             </div>
 
-            <Typography id="graphInfo"></Typography>
+            <Typography style={{position: "absolute", bottom: 0, marginBottom: 30, marginLeft: 16 }} id="graphInfo"></Typography>
+        </React.Fragment>
 
-        </div>
     )
 }
 
