839b3d3a84d5c35a2d452a2cf32ce71d46d19304
croftr <rob@robincroft.com>
2020-04-03 16:42:36 +0100

    tweak progress bar

--- src/visGraph/VisGraphViewer.jsx
+++ src/visGraph/VisGraphViewer.jsx
@@ -5,8 +5,7 @@ import { changeLayout } from "./visGraph.js"
 
 export default function VisGraph({ graphData, network, height }) {
 
-    React.useEffect(() => {
-        console.log("use effect go ");        
+    React.useEffect(() => {        
     }, []);
 
     const arrange = () => {
@@ -15,9 +14,9 @@ export default function VisGraph({ graphData, network, height }) {
 
     return (
         <React.Fragment>
-            <div id="loadingBar" style={{ position: "absolute ", left: "50%", bottom: "50%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <div id="loadingBar" style={{ position: "absolute ", left: "50%", bottom: "50%", display: graphData ? "none" : "flex", flexDirection: "column", alignItems: "center" }}>
                 <Typography style={{ marginBottom: 16 }}>Loading Graph</Typography>
-                <div class="loader"></div>
+                <div className="loader"></div>
             </div>
             <div style={{ position: "absolute", top: 0, marginTop: 134 + network.offsetHeight, marginLeft: 4, zIndex: 999, display: "flex", alignItems: "center", justifyContent: "space-between" }} id="graphTools">
                 <div id="tools" style={{ display: "flex", alignItems: "center" }}>
