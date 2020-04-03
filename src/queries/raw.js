1fc34aafc53ff4b4ee8d42889d7469ccd4ea26f6
croftr <rob@robincroft.com>
2020-04-03 14:30:45 +0100

    remove country payload

--- src/queries/raw.js
+++ src/queries/raw.js
@@ -2,47 +2,47 @@ import { execute } from "../actions/GafferActions"
 
 export const convertRaw = async (edgeType) => {
 
-    // const payload = {
-    //     class: "uk.gov.gchq.gaffer.operation.OperationChain",
-    //     operations: [{
-    //         class: "uk.gov.gchq.gaffer.operation.impl.get.GetAllElements"
-    //     }, {
-    //         class: "uk.gov.gchq.gaffer.operation.impl.Limit",
-    //         resultLimit: 1000,
-    //         truncate: true
-    //     }]
-    // }
-
     const payload = {
-        "class": "uk.gov.gchq.gaffer.operation.impl.get.GetElements",
-        "input": [
-          {
-            "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
-            "type": "country",      
-            "value": "Austria"
-          },
-          {
-            "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
-            "type": "country",      
-            "value": "Russia"
-          },
-          {
-            "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
-            "type": "country",      
-            "value": "Thailand"
-          },
-          {
-            "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
-            "type": "country",      
-            "value": "Norway"
-          },                    
-          {
-            "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
-            "type": "country",      
-            "value": "United Kingdom1"
-          },   
-        ]
-      }
+        class: "uk.gov.gchq.gaffer.operation.OperationChain",
+        operations: [{
+            class: "uk.gov.gchq.gaffer.operation.impl.get.GetAllElements"
+        }, {
+            class: "uk.gov.gchq.gaffer.operation.impl.Limit",
+            resultLimit: 1000,
+            truncate: true
+        }]
+    }
+
+    // const payload = {
+    //     "class": "uk.gov.gchq.gaffer.operation.impl.get.GetElements",
+    //     "input": [
+    //       {
+    //         "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
+    //         "type": "country",      
+    //         "value": "Austria"
+    //       },
+    //       {
+    //         "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
+    //         "type": "country",      
+    //         "value": "Russia"
+    //       },
+    //       {
+    //         "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
+    //         "type": "country",      
+    //         "value": "Thailand"
+    //       },
+    //       {
+    //         "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
+    //         "type": "country",      
+    //         "value": "Norway"
+    //       },                    
+    //       {
+    //         "class": "uk.gov.gchq.gaffer.types.TypeSubTypeValue",
+    //         "type": "country",      
+    //         "value": "United Kingdom1"
+    //       },   
+    //     ]
+    //   }
     
     if (edgeType) {
         payload.view = {
