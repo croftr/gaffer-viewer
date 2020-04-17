95b2f8d74d0c91f7d1c19524fc8dfa3b902a3f5c
croftr <rob@robincroft.com>
2020-04-17 18:53:17 +0100

    allow delete  graph from manage graphs page

--- src/manageGraphs/ManageGraphsPage.jsx
+++ src/manageGraphs/ManageGraphsPage.jsx
@@ -13,14 +13,17 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import GraphIcon from '@material-ui/icons/Storage';
-import DeleteIcon from '@material-ui/icons/HighlightOff';
+import DeleteIcon from '@material-ui/icons/Delete';
+import LoadIcon from '@material-ui/icons/Publish';
 import JSONPretty from 'react-json-pretty';
 
 
 export default function ManageGraphs({ graphs, loadGraph, schema, onDeleteGraph }) {
 
     const [isOpen, setIsOpen] = useState(false);
-    const [selectedGraph, setSelectedGraph] = React.useState();
+    const [graphToDelete, setGraphToDelete] = useState(false);
+    const [isDeleteGraphOpen, setIsDeleteGraphOpen] = useState(false);
+    const [selectedGraph, setSelectedGraph] = useState();
 
     const loadSelectedGraph = (graph) => {
         setSelectedGraph(graph);
@@ -30,27 +33,77 @@ export default function ManageGraphs({ graphs, loadGraph, schema, onDeleteGraph 
     const deleteGraph = (selectedGraph) => {
         setSelectedGraph(undefined)
         onDeleteGraph(selectedGraph);
+        setIsDeleteGraphOpen(false);
+    }
+
+    const loadData = (selectedGraph) => {
+    }
+
+    const openDeleteGraph = (selectedGraph) => {
+        setIsDeleteGraphOpen(true);
+        setGraphToDelete(selectedGraph)
     }
 
     return (
-        <Paper style={{ height: "100%", padding: 16 }}>
-            <Typography>My Graphs</Typography>
+        <>
+            <Paper style={{ height: "100%", padding: 16, display: "flex" }}>
 
-            <List style={{ height: "calc(100vh - 130px)", overflowY: "auto", width: 320, marginRight: 8 }}>
-                {["All"].concat(graphs).map(graph => (
-                    <ListItem button onClick={() => loadSelectedGraph(graph)} key={graph}>
-                        <ListItemAvatar>
-                            <Avatar>
-                                <GraphIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary={graph} />
-                    </ListItem>
-                ))}
-            </List>
+                <List style={{ height: "calc(100vh - 130px)", overflowY: "auto", width: 320, marginRight: 8 }}>
+                    {graphs.map(graph => (
+                        <ListItem button onClick={() => loadSelectedGraph(graph)} key={graph}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <GraphIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={graph} />
+                        </ListItem>
+                    ))}
+                </List>
 
-            <Dialog aria-labelledby="simple-dialog-title" open={isOpen} fullScreen>
-                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                {selectedGraph &&
+                    <div id="graphToManage">
+                        <div id="manageGraphsHeader" style={{ display: "flex", alignItems: "center", height: 48, justifyContent: "space-between" }}>
+                            <Typography variant="h6">{selectedGraph}</Typography>
+                            <div className="manageGraphButtons">
+                                <Button
+                                    color="primary"
+                                    variant="contained"
+                                    startIcon={<LoadIcon />}
+                                    onClick={() => loadData(selectedGraph)}>
+                                    Load Data
+                                </Button>
+                                <Button
+                                    color="secondary"
+                                    variant="contained"
+                                    startIcon={<DeleteIcon />}
+                                    onClick={() => openDeleteGraph(selectedGraph)}
+                                    style={{ marginLeft: 16 }}>
+                                    Delete
+                                </Button>
+                            </div>
+                        </div>
+                        <JSONPretty
+                            style={{
+                                border: "1px solid lightGrey",
+                                padding: 8
+                            }}
+                            id="json-pretty"
+                            data={schema} >
+                        </JSONPretty>
+                    </div>
+                }
+
+                <Fab variant="extended" color="primary" aria-label="add" onClick={() => setIsOpen(true)} style={{ position: "absolute", bottom: 48, right: 48 }} >
+                    <AddIcon style={{ marginRight: 8 }} />
+                    <span style={{ padding: 8 }}>Add Graph</span>
+                </Fab>
+
+            </Paper>
+
+            <Dialog aria-labelledby="createGraphDialog" open={isOpen} fullScreen>
+
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
                     <DialogTitle id="simple-dialog-title">Add Graph</DialogTitle>
                     <IconButton onClick={() => setIsOpen(false)}>
                         <CloseIcon />
@@ -60,12 +113,28 @@ export default function ManageGraphs({ graphs, loadGraph, schema, onDeleteGraph 
                 <CreateGraphPage />
             </Dialog>
 
-            <Fab variant="extended" color="primary" aria-label="add" onClick={() => setIsOpen(true)} style={{ position: "absolute", bottom: 48, right: 48 }} >
-                <AddIcon style={{ marginRight: 8 }} />
-                <span style={{ padding: 8 }}>Add Schema</span>
-            </Fab>
+            <Dialog aria-labelledby="deleteGraphDialog" open={isDeleteGraphOpen} >
 
-        </Paper>
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                    <DialogTitle id="simple-dialog-title">Delete {graphToDelete}</DialogTitle>
+                    <IconButton onClick={() => setIsDeleteGraphOpen(false)}>
+                        <CloseIcon />
+                    </IconButton>
+                </div>
+                <Typography>Enter name of graph to confirm delete</Typography>
+                <TextField></TextField>
+
+                <Button
+                    color="secondary"
+                    variant="contained"
+                    startIcon={<DeleteIcon />}
+                    onClick={() => deleteGraph(graphToDelete)}
+                    style={{ marginLeft: 16 }}>
+                    Delete
+                </Button>
+            </Dialog>
+
+        </>
     )
 
 }
