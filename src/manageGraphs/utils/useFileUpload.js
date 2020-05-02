f44fa6e67799dcf63e64cc8bff16f26a70ba500f
croftr <rob@robincroft.com>
2020-05-02 11:50:56 +0100

    remove file upload custom hook

--- src/manageGraphs/utils/useFileUpload.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-export const useFileUpload = () => {
-    const [file, setFile] = useState("");
-    const [filename, setFilename] = useState('');
-
-    return file;
-}
